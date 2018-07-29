c5b5084c136f329abba59a21a37c322536e251b2
GRAND MASTER COİN <38991843+grandmastercoin@users.noreply.github.com>
2018-07-29 14:11:24 +0300

    Reset metadata on upgrade to v0.7.6.2

--- upgrade-mn-0.7.6.2.sh
+++ upgrade-mn-0.7.6.2.sh
@@ -8,6 +8,11 @@ sudo wget https://github.com/grandmastercoin/grandmastercoin/releases/download/v
 sudo tar -xzvf /home/grandmastercoin/grandmastercoin-0.7.6.2-cli-linux.tar.gz -C /home/grandmastercoin/
 sudo chown -R grandmastercoin:grandmastercoin /home/grandmastercoin/grandmastercoin* && sudo chmod 755 /home/grandmastercoin/grandmastercoin*
 sudo rm /home/grandmastercoin/grandmastercoin-0.7.6.2-cli-linux.tar.gz
+# reset metadata to free maxconnections slots for nodes
+# with recent version, as their metadata are not compatible
+sudo rm /home/grandmastercoin/.grandmastercoincore/mncache.dat
+sudo rm /home/grandmastercoin/.grandmastercoincore/mnpayments.dat
+sudo rm /home/grandmastercoin/.grandmastercoincore/peers.dat
 sudo systemctl start grandmastercoin
 
 echo "Masternode upgrade complete!"
